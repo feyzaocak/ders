0df5793fa45597674ec548d366cb24285173035c
feyzaocak <feyzaocak@icloud.com>
2022-02-23 11:37:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, result;
+            float floatresult;
+            x = 7;
+            y = 5;
+
+            result = x + y;
+            Console.WriteLine("x+y:{0}",result);
+            result = x - y;
+            Console.WriteLine("x-y:{0}",result);
+            result = x * y;
+            Console.WriteLine("x*y:{0}",result);
+            result = x / y;
+            Console.WriteLine("x/y:{0}",result);
+            floatresult = (float)x / (float)y;
+            Console.WriteLine("x/y:{0}",floatresult);
+            result = x % y;
+            Console.WriteLine("x%y:{0}",result);
+            result += x;
+            Console.WriteLine("result +x:{0}",result);
+            Console.ReadKey();
+        }
+    }
+}
